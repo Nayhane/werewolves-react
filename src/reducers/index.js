fa18aad1563a436373d84e12e7d8f44c8159d08d
ANISSA <anissamiloudi@hotmail.nl>
2018-02-27 17:59:53 +0100

    Removed all the village routes because we dont need it

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,11 @@ import loading from './loading'
 import loadError from './loadError'
 import currentUser from './currentUser'
 import players from './players'
-import villages from './villages'
 console.log(players)
 
 export default {
   loading,
   loadError,
   currentUser,
-  players,
-  villages
+  players
 }
