eaf55fbb0a2131197341d229f95457a1a4727f16
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-02-28 08:51:51 +0100

    Added 2 more timers to play around with

--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -6,6 +6,8 @@ import { push } from 'react-router-redux'
 import { connect as subscribeToWebsocket } from '../actions/websocket'
 import Sidebar from './Sidebar'
 import Timer from '../components/games/Timer'
+import AnotherTimerAgain from '../components/games/AnotherTimerAgain'
+
 import Paper from 'material-ui/Paper'
 import './Lobby.css'
 
@@ -22,8 +24,9 @@ class Lobby extends PureComponent {
     return (
       <div className="Lobby">
         <RegisterPlayer />
-        <Paper className="paper">
+        <AnotherTimerAgain />
         <Timer />
+        <Paper className="paper">
         <Sidebar/>
         </Paper>
       </div>
