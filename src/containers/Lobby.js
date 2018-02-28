51e11d12d7f402587e30b79ec5be3963df90552b
Erlemorgaine <erle_3@hotmail.com>
2018-02-28 09:59:32 +0100

    onesidebar

--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -24,6 +24,7 @@ class Lobby extends PureComponent {
         <RegisterPlayer />
         <Paper className="paper">
         <Timer />
+        //only one sidebar
         <Sidebar/>
         </Paper>
       </div>
