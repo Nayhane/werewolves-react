a03d699fb39549a04b1d6d7a3251c3869122ccd9
Erlemorgaine <erle_3@hotmail.com>
2018-03-02 10:17:28 +0100

    not important

---
+++ src/containers/MessagePage.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class MessagePage extends PureComponent {
+
+  render() {
+    return(
+      <div>Hello</div>
+    )
+  }
+
+}
+
+export default MessagePage
