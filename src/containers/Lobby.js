4204aa4b1a0c727b084ba3d203e82c699d1ddbd3
Schmarfy <o.vanzijverden@gmail.com>
2018-03-05 12:01:37 +0100

    Add pop-over for sending messages

--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -5,21 +5,11 @@ import { push } from 'react-router-redux'
 import Sidebar from './Sidebar'
 import Timer from '../components/games/Timer'
 import VillageAvatar from './VillageAvatar'
-import MessageBox from '../components/games/MessageBox'
 import Paper from 'material-ui/Paper'
 import './Lobby.css'
 
 class Lobby extends PureComponent {
   componentWillMount() {
-  }
-
-
-  renderMessageBox = (player, index) => {
-    if (player.messageSent === 'sending') {
-      return(
-        <MessageBox key={index} players={ this.props.players } player = {player}/>
-      )
-    }
   }
 
   render() {
@@ -28,10 +18,7 @@ class Lobby extends PureComponent {
         <Paper className="paper">
           <Sidebar className="sidebar"/>
           <Timer />
-          { this.props.players.map(this.renderMessageBox) }
-
           <VillageAvatar players={this.props.players}/>
-
         </Paper>
       </div>
 
