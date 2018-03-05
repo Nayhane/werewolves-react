53da52e9a1552eaac385adc3e9b25447f3bd6508
ANISSA <anissamiloudi@hotmail.nl>
2018-03-05 18:41:19 +0100

    Made new buttons and working new page

---
+++ src/containers/wakkerdamMessagePage.js
@@ -0,0 +1,71 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { connect as subscribeToWebsocket } from '../actions/websocket'
+import MessageItem from '../components/games/MessageItem.js'
+
+import { fetchPlayers} from '../actions/games/fetch'
+
+class wakkerdamMessagePage extends PureComponent {
+
+  componentWillMount() {
+    this.props.fetchPlayers()
+    this.props.subscribeToWebsocket()
+  }
+
+  renderMessage = (message, index) => {
+    return(
+      <MessageItem key={index} message={message}/>
+    )
+  }
+
+  render() {
+    const wakkerdamRecipients = this.props.players.filter((player)=> {
+    return player.village[0].name === "Wakkerdam"
+  })
+
+    const recipients = wakkerdamRecipients.filter((player) => {
+      return player.receivedMessages.length > 0
+    })
+
+    const messages = recipients.map((recipient) => {
+      const allMessages = recipient.receivedMessages
+      return [recipient, allMessages]
+    })
+
+    const singleMessages = messages.map((message) => {
+      const singleRecipient = message[1].map((m) => {
+        return {...m, recipient: message[0]}
+      })
+
+      return singleRecipient
+    })
+
+    const allMessages = [].concat.apply([], singleMessages)
+
+    allMessages.sort((a, b) => {
+      const keyA = a.createdAt
+      const keyB = b.createdAt
+
+      if(keyA < keyB) return 1;
+      if(keyA > keyB) return -1;
+      return 0
+    })
+
+    return(
+      <div className="message-page" style={{ margin: 30 }}>
+        { allMessages.map(this.renderMessage) }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser, players }) => {
+  return {
+    players
+  }
+}
+
+export default connect(mapStateToProps, {
+  subscribeToWebsocket,
+  fetchPlayers
+})(wakkerdamMessagePage)
