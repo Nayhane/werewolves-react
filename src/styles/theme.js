74d6a17559db97386cb9a822115254816fa90d68
ANISSA <anissamiloudi@hotmail.nl>
2018-03-07 13:50:14 +0100

    changed the pop-ups

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -28,7 +28,7 @@ export const palette = {
 
  accent1Color: secondaryColor,
  textColor: lightGrey,
- alternateTextColor: lightGrey,
+ alternateTextColor: greyBlue,
 
  canvasColor: primary1Color,
  borderColor: secondaryDarkColor,
